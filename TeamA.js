11bf4be9a752cca0b479144ac5cbb2ce023afd99
Lorenzo Zannetti <zannetti.lorenzo@gmail.com>
2024-05-17 16:14:29 +0200

    minor fix

--- TeamA.js
+++ TeamA.js
@@ -1,1 +1,18 @@
-const project1 = ["p"];
+let gente = [];
+
+let newPerson = {
+  name: "Paolo",
+  surname: "Rossi",
+  age: 30,
+  city: "New York",
+  hobby: "Leggere",
+  favoriteFood: "Booh",
+  favoriteVideoGame: "The Legend of Zelda",
+  favoriteFilm: "Inception",
+  favoriteBook: "1984",
+  petName: "Fido",
+};
+
+gente.push(newPerson);
+
+console.log(gente);
