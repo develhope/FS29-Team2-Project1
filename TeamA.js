c383b6cbc687c8f3af10b70783180b88bbe47ac9
Lorenzo Zannetti <zannetti.lorenzo@gmail.com>
2024-05-19 15:46:29 +0200

    array popolato

--- TeamA.js
+++ TeamA.js
@@ -1,18 +1,55 @@
-let gente = [];
+let gente = [
+  {
+    name: "Paolo",
+    surname: "Rossi",
+    age: 30,
+    city: "New York",
+    hobby: "Leggere",
+    favoriteFood: "Booh",
+    favoriteVideoGame: "The Legend of Zelda",
+    favoriteFilm: "Inception",
+    favoriteBook: "1984",
+    petName: "Fido",
+  },
+  {
+    name: "Giuseppe",
+    surname: "Verdi",
+    age: 35,
+    city: "Londra",
+    hobby: "Palestra",
+    favoriteFood: "Sushi",
+    favoriteVideoGame: "Pokemon",
+    favoriteFilm: "Boris",
+    favoriteBook: "Lord of the rings",
+    petName: "Pippo",
+  },
+  {
+    name: "Lorenzo",
+    surname: "Neri",
+    age: 60,
+    city: "Amsterdam",
+    hobby: "Programmare",
+    favoriteFood: "Kebab",
+    favoriteVideoGame: "Final Fantasy",
+    favoriteFilm: "Casablanca",
+    favoriteBook: "1984",
+    petName: "Fido",
+  },
+];
+// - Print the team middle age.
+// - Print who has a pet (name petName).
 
-let newPerson = {
-  name: "Paolo",
-  surname: "Rossi",
-  age: 30,
-  city: "New York",
-  hobby: "Leggere",
-  favoriteFood: "Booh",
-  favoriteVideoGame: "The Legend of Zelda",
-  favoriteFilm: "Inception",
-  favoriteBook: "1984",
-  petName: "Fido",
-};
-
-gente.push(newPerson);
-
-console.log(gente);
+// let newPerson = {
+//   name: "Paolo",
+//   surname: "Rossi",
+//   age: 30,
+//   city: "New York",
+//   hobby: "Leggere",
+//   favoriteFood: "Booh",
+//   favoriteVideoGame: "The Legend of Zelda",
+//   favoriteFilm: "Inception",
+//   favoriteBook: "1984",
+//   petName: "Fido",
+// };
+// gente.push(newPerson);
+// console.log(gente);
