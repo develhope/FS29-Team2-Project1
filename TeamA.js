05909a6d3dc6a7f64bc24bb262dc9b7ace2db2d1
Fabrizio <faber96@gmail.com>
2024-05-20 15:02:31 +0200

    new fix

--- TeamA.js
+++ TeamA.js
@@ -1,1 +1,15 @@
-const project1 = [];
+const project1 = [{
+
+    name:'Fabrizio',
+    surname: 'Geraci',
+    age: 28,
+    city: 'Palermo',
+    hobby: 'Videogames',
+    favoriteFood: 'Pizza',
+    favoriteVideoGame: 'Genshin Impact',
+    favoriteFilm: 'LOTR',
+    favoriteBook: 'A Silent Voice',
+    petName: '',
+
+}];
+
